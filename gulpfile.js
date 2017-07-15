1b842e1b03d142787e52a29f2fd07a89684398cc
Jacob <jacyager@gmail.com>
2017-07-15 00:21:09 -0400

    Changed sass folder name in atempt to solve susy error

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-  return gulp.src('./sass/main.sass') // Gets all files ending with .sass in app/sass
+  return gulp.src('./sassFiles/main.sass') // Gets all files ending with .sass in app/sass
     .pipe(sass({
           outputStyle: 'compressed',
           includePaths: ['node_modules/susy/sass']
@@ -16,7 +16,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
-  gulp.watch('./sass/*.sass', ['sass']);
+  gulp.watch('./sassFiles/*.sass', ['sass']);
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('./pages/*.html', browserSync.reload);
   gulp.watch('./index.html', browserSync.reload);

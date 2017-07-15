3559cfd930697d0d192a6859517ca9ad729b0a9a
Jacob <jacyager@gmail.com>
2017-07-15 01:06:32 -0400

    I have destroyed susy, it is gone

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,25 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 
+// gulp.task('sass', function() {
+//   return gulp.src('./sassFiles/main.sass') // Gets all files ending with .sass in app/sass
+//     .pipe(sass({
+//           outputStyle: 'compressed',
+//           includePaths: ['node_modules/susy/sass']
+//       }).on('error', sass.logError))
+//     .pipe(gulp.dest('./'))
+//     .pipe(browserSync.reload({
+//       stream: true
+//     }))
+// });
+
 gulp.task('sass', function() {
-  return gulp.src('./sassFiles/main.sass') // Gets all files ending with .sass in app/sass
-    .pipe(sass({
+  return gulp.src('sassFiles/*.sass')
+      .pipe(sass({
           outputStyle: 'compressed',
           includePaths: ['node_modules/susy/sass']
       }).on('error', sass.logError))
-    .pipe(gulp.dest('./'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+      .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
